de02e10366d33376ecab907132c55b8842bf3f6f
zahrabateninia <zahrabatenin@gmail.com>
2025-02-02 22:53:39 +0330

    Write two tests for isSunk() and fail them

--- __tests__/ship.test.js
+++ __tests__/ship.test.js
@@ -10,5 +10,20 @@ describe('Ship', () => {
     const ship = new Ship(3)
     ship.hit()
     expect(ship.hits).toBe(1)
-  })
+  });
+
+  test('isSunk() returns true when hits equal length', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+  
+  test('isSunk() returns false when hits are less than length', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.isSunk()).toBe(false);
+  });
+  
 });
