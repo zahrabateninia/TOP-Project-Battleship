37580ffa7c333ef1458d1b4d948bce2b759f5fce
zahrabateninia <zahrabatenin@gmail.com>
2025-02-03 09:09:29 +0330

    Implement receiveAttack() method

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -1,16 +1,25 @@
-class Gameboard{
-    constructor(){
-        this.ships = []
+class Gameboard {
+    constructor() {
+      this.ships = [];
+      this.missedShots = []; 
     }
-
-    placeShip(ship, position){
-        ship.position = position
-        this.ships.push(ship)
-    }
-
-
-}
   
-module.exports = Gameboard;
-
+    placeShip(ship, position) {
+      ship.position = position;
+      this.ships.push(ship);
+    }
+  
+    receiveAttack(coord) {
+      for (let ship of this.ships) {
+        if (JSON.stringify(ship.position) === JSON.stringify(coord)) {
+          ship.hit();
+          return;
+        }
+      }
+      this.missedShots.push(coord); // If no ship is hit, record a miss
+    }
+  }
+  
+  module.exports = Gameboard;
+  
 
